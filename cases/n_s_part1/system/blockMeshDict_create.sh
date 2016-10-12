583ebb5b654de98d405895d80e80c797d9f0f5dd
Shupachet <shupachet@gmail.com>
2016-10-12 20:03:06 +0300

    12.10.2016

--- cases/n_s_part1/system/blockMeshDict_create.sh
+++ cases/n_s_part1/system/blockMeshDict_create.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+# Params
 # Hole
-HoleRadiusOuter=0.5
-HoleRadiusOuterIpoint="0"`echo "scale=18; $HoleRadiusOuter / sqrt(2)" | bc`;
-HoleRadiusInner=0.45
-HoleRadiusInnerIpoint="0"`echo "scale=18; $HoleRadiusInner / sqrt(2)" | bc`;
+HoleRadius=0.5
+HoleRadiusIpoint="0"`echo "scale=18; $HoleRadius / sqrt(2)" | bc`;
 HoleHeight=10
 # Imposion cell
-ICRadiusOuter=0.2
-ICRadiusOuterIpoint="0"`echo "scale=18; $ICRadiusOuter / sqrt(2)" | bc`;
+ICRadius=0.2
+ICRadiusIpoint="0"`echo "scale=18; $ICRadius / sqrt(2)" | bc`;
 ICHeight=0.8
 ICInitIndentZ=0.1
-
+# Layer
+LayerRadius=7
+LayerHeight=2
 # Additional values
 ICBottomZ=$ICInitIndentZ
 ICTopZ="0"`echo $ICInitIndentZ + $ICHeight | bc`;
@@ -37,100 +38,108 @@ FoamFile
 //Main points list
 vertices
 (
-	//Hole Outer Bottom 0-3
-	( $HoleRadiusOuter 0 0 )
-	( 0 $HoleRadiusOuter 0 )
-	( -$HoleRadiusOuter 0 0 )
-	( 0 -$HoleRadiusOuter 0 )
-	//Hole Outer Top 4-7
-	( $HoleRadiusOuter 0 $HoleHeight )
-	( 0 $HoleRadiusOuter $HoleHeight )
-	( -$HoleRadiusOuter 0 $HoleHeight )
-	( 0 -$HoleRadiusOuter $HoleHeight )
-	//Hole Inner Bottom 8-11
-	( $HoleRadiusInner 0 0 )
-	( 0 $HoleRadiusInner 0 )
-	( -$HoleRadiusInner 0 0 )
-	( 0 -$HoleRadiusInner 0 )
-	//Hole Inner Top 12-15
-	( $HoleRadiusInner 0 $HoleHeight )
-	( 0 $HoleRadiusInner $HoleHeight )
-	( -$HoleRadiusInner 0 $HoleHeight )
-	( 0 -$HoleRadiusInner $HoleHeight )
+	//Hole TOP : 0-3
+	( $HoleRadius 0 $HoleHeight )
+	( 0 $HoleRadius $HoleHeight )
+	( -$HoleRadius 0 $HoleHeight )
+	( 0 -$HoleRadius $HoleHeight )
+	//ICell TOP : 4-7
+	( $ICRadius 0 $ICTopZ )
+	( 0 $ICRadius $ICTopZ )
+	( -$ICRadius 0 $ICTopZ )
+	( 0 -$ICRadius $ICTopZ )
+	//ICell BOTTOM : 8-11
+	( $ICRadius 0 $ICBottomZ )
+	( 0 $ICRadius $ICBottomZ )
+	( -$ICRadius 0 $ICBottomZ )
+	( 0 -$ICRadius $ICBottomZ )
+	//Hole BOTTOM : 12-15
+	( $HoleRadius 0 0 )
+	( 0 $HoleRadius 0 )
+	( -$HoleRadius 0 0 )
+	( 0 -$HoleRadius 0 )
+	//Layer TOP : 16-19
+	( $LayerRadius 0 0 )
+	( 0 $LayerRadius 0 )
+	( -$LayerRadius 0 0 )
+	( 0 -$LayerRadius 0 )
+	//Layer BOTTOM : 20-23
+	( $LayerRadius 0 -$LayerHeight )
+	( 0 $LayerRadius -$LayerHeight )
+	( -$LayerRadius 0 -$LayerHeight )
+	( 0 -$LayerRadius -$LayerHeight )
 );
 
 //Arcs to full cirle
 edges
 (
-	//Hole Outer Bottom 0-3
-	arc 0 1 ($HoleRadiusOuterIpoint $HoleRadiusOuterIpoint 0)
-	arc 1 2 (-$HoleRadiusOuterIpoint $HoleRadiusOuterIpoint 0)
-	arc 2 3 (-$HoleRadiusOuterIpoint -$HoleRadiusOuterIpoint 0)
-	arc 3 0 ($HoleRadiusOuterIpoint -$HoleRadiusOuterIpoint 0)
-	//Hole Outer Top 4-7
-	arc 4 5 ($HoleRadiusOuterIpoint $HoleRadiusOuterIpoint $HoleHeight)
-	arc 5 6 (-$HoleRadiusOuterIpoint $HoleRadiusOuterIpoint $HoleHeight)
-	arc 6 7 (-$HoleRadiusOuterIpoint -$HoleRadiusOuterIpoint $HoleHeight)
-	arc 7 4 ($HoleRadiusOuterIpoint -$HoleRadiusOuterIpoint $HoleHeight)
-	//Hole Inner Bottom 8-11
-	arc 8 9 ($HoleRadiusInnerIpoint $HoleRadiusInnerIpoint 0)
-	arc 9 10 (-$HoleRadiusInnerIpoint $HoleRadiusInnerIpoint 0)
-	arc 10 11 (-$HoleRadiusInnerIpoint -$HoleRadiusInnerIpoint 0)
-	arc 11 8 ($HoleRadiusInnerIpoint -$HoleRadiusInnerIpoint 0)
-	//Hole Inner Top 12-15
-	arc 12 13 ($HoleRadiusInnerIpoint $HoleRadiusInnerIpoint $HoleHeight)
-	arc 13 14 (-$HoleRadiusInnerIpoint $HoleRadiusInnerIpoint $HoleHeight)
-	arc 14 15 (-$HoleRadiusInnerIpoint -$HoleRadiusInnerIpoint $HoleHeight)
-	arc 15 12 ($HoleRadiusInnerIpoint -$HoleRadiusInnerIpoint $HoleHeight)
+	//Hole TOP : 0-3
+	arc 0 1 ($HoleRadiusIpoint $HoleRadiusIpoint $HoleHeight)
+	arc 1 2 (-$HoleRadiusIpoint $HoleRadiusIpoint $HoleHeight)
+	arc 2 3 (-$HoleRadiusIpoint -$HoleRadiusIpoint $HoleHeight)
+	arc 3 0 ($HoleRadiusIpoint -$HoleRadiusIpoint $HoleHeight)
+	//ICell TOP : 4-7
+	arc 4 5 ($ICRadiusIpoint $ICRadiusIpoint $ICTopZ)
+	arc 5 6 (-$ICRadiusIpoint $ICRadiusIpoint $ICTopZ)
+	arc 6 7 (-$ICRadiusIpoint -$ICRadiusIpoint $ICTopZ)
+	arc 7 4 ($ICRadiusIpoint -$ICRadiusIpoint $ICTopZ)
+	//ICell BOTTOM : 8-11
+	arc 8 9 ($ICRadiusIpoint $ICRadiusIpoint $ICBottomZ)
+	arc 9 10 (-$ICRadiusIpoint $ICRadiusIpoint $ICBottomZ)
+	arc 10 11 (-$ICRadiusIpoint -$ICRadiusIpoint $ICBottomZ)
+	arc 11 8 ($ICRadiusIpoint -$ICRadiusIpoint $ICBottomZ)
+	//Hole BOTTOM : 12-15
+	arc 12 13 ($HoleRadiusIpoint $HoleRadiusIpoint 0)
+	arc 13 14 (-$HoleRadiusIpoint $HoleRadiusIpoint 0)
+	arc 14 15 (-$HoleRadiusIpoint -$HoleRadiusIpoint 0)
+	arc 15 12 ($HoleRadiusIpoint -$HoleRadiusIpoint 0)
 );
 
 blocks
 (
-	//Hole Outer 0-7
-//	hex (0 1 2 3 4 5 6 7) (30 30 30) simpleGrading (1 1 10)
-	//Hole Inner 8-15
-//	hex (8 9 10 11 12 13 14 15) (30 30 30) simpleGrading (1 1 10)
-	//Sectors
-	hex (0 1 9 8 4 5 13 12) (30 30 30) simpleGrading (1 1 10)
-	hex (1 2 10 9 5 6 14 13) (30 30 30) simpleGrading (1 1 10)
-	hex (2 3 11 10 6 7 15 14) (30 30 30) simpleGrading (1 1 10)
-	hex (3 0 8 11 7 4 12 15) (30 30 30) simpleGrading (1 1 10)
+
+	//Hole
+	hex (12 15 14 13 0 3 2 1) (30 30 30) simpleGrading (1 1 10) 
+	//Implosion Cell
+	hex (4 7 6 5 8 11 10 9) (30 30 30) simpleGrading (1 1 10)
+	//Layer
+	hex (16 19 18 17 20 23 22 21) (30 30 30) simpleGrading (1 1 10)
 );
 
 patches
 (
-/*
-	//REPAIR HERE! Hole Outer 0-7 INner 8-15
-	patch holeinnerborder
-	(
-		(8 12 13 9)
-		(9 13 14 10)
-		(10 14 15 11)
-		(11 15 12 8)
-	)
-	patch holeoutertop
-	(
-		(4 7 6 5)
-	)
-	patch holeouterbottom
+	//Hole TOP / Hole AREA1 TOP : 0-3
+	patch holetop
 	(
 		(0 3 2 1)
 	)
-	patch holeinnertop
+	//Hole INNER BODY : 0-3 + 12-15
+	patch holeinnerbody
 	(
-		(8 11 10 9)
+		(12 0 1 13)
+		(13 1 2 14)
+		(14 2 3 15)
+		(15 3 0 12)
 	)
-	patch holeinnerbottom
+	//Hole BOTTOM : 12-15
+	patch holebottom
 	(
 		(12 15 14 13)
 	)
-*/
+	//ICell OUTER BODY
+	patch icouterbody
+	(
+		(8 4 5 9)
+		(9 5 6 10)
+		(10 6 7 11)
+		(11 7 4 8)
+		(4 5 6 7)
+		(8 9 10 11)
+	)
 );
 
 mergePatchPairs
 (
-//( holeouterborder holeinnerborder )
-//( holeoutertop holeinnertop )
 );
 
 // ************************************************************************* //
